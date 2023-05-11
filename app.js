29e8f95cc2aa75f7c50b4b5f3d9ba93f5acc0063
Pablo Oliveira <pablue@gmail.com>
2023-05-11 11:21:58 -0400

    voltando ao ponto terceiro console log

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 // mais console log de teste:
 console.log("O Pablo fez um commit extra")
-console.log("Enviando paa o GitHub")
